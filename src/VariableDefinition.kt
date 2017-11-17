b1324c40f84d95cd705e883e13c34162aa5f5ba8
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-17 15:59:23 -0200

    Variables Definition

---
+++ src/VariableDefinition.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+    /*
+    Variables are "places" where you can save your voodoo
+    LOL
+
+    var {mutable}
+    val {immutable}
+     */
+     var firstName = "Hecton" //you can change it later
+     val fullName = "Hecton Domingos" // you cannot change it later
+     var age = 31
+
+    //there is two ways to concatenates a string
+    println("You can concatenate using the kotlin way $firstName")
+    println("You also can concatenate like that " + fullName)
+
+
+
+    println("My name is $firstName")
+    println ("My name is $fullName" + "and I am " + age + "years old")
+
+}
