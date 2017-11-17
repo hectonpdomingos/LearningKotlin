52d7f5644f0dba1d84d419d2b882678f7fdab058
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-17 16:38:44 -0200

    Variable Float

---
+++ src/VariableFloat.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+    /*
+    Variable Float example: 0.9 - 1.8
+     */
+
+    var myFloat = 0.3
+    var newFloat: Float? = null
+
+    //You need to put F after to sign float number, bc not
+    //it will be double
+
+    newFloat = 31.9F
+    var float: Float = 0.2F
+
+
+    println("The variable myFloat kotlin assumes be double $myFloat")
+    println("The variable newFloat kotlin knows that is float  $newFloat")
+
+}
