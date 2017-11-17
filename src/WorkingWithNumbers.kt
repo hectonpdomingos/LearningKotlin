32e664192bbae3a5107e1366658e551fa40604f1
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-17 17:18:28 -0200

    Working with numbers

--- src/WorkingWithNumbers.kt
+++ src/WorkingWithNumbers.kt
@@ -1,3 +1,23 @@
 fun main(args: Array<String>) {
-    
+
+    /*
+
+     */
+    println("############ Adding numbers ###############")
+
+    var firstNum = 20
+    var secondNumber = 30
+
+    var result: Int?
+    result = firstNum + secondNumber
+
+    println("The sum of first and second number is $result")
+
+    println("If you want to make the math use { between numbers {$firstNum + $secondNumber}")
+
+    println("############Subtration ###############")
+
+
+
+
 }
