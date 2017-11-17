2afe3fb36699a7ba58c3945fcd2e7e57f5edf186
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-17 16:30:34 -0200

    Variable Boolean

---
+++ src/VariableBool.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+    /*
+    Variable boolean
+    true OR false
+    You are awesome Hecton - True
+     */
+
+    //first way to declare bool
+    var youAreAwesome = true
+
+    // second way to declare bool
+    var youAreRight:Boolean = false
+
+    //in case you want to set the value after
+    var newPool: Boolean? = null
+    newPool = true
+    println("Are you Awesome, Hecton? $youAreAwesome")
+
+    println("Are you Right, Hecton? $youAreRight")
+
+    println("Do you want a new pool? $newPool")
+
+
+}
