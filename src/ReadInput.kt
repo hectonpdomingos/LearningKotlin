29b19464e7b4ace16e8f870b3ce57f0667a7f1ec
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-17 16:55:35 -0200

    Get input from user

---
+++ src/ReadInput.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    /*
+    read inputs from users
+     */
+    println("Type the value you want to be stored")
+    var value = readLine()
+    println("You just type $value")
+}
