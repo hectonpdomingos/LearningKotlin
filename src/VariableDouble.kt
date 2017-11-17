c3753f6ebc9e0ec594641fa19b45b77ecbebdb24
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-17 16:22:25 -0200

    Variable Double

---
+++ src/VariableDouble.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    /*
+    Double type: Example 31.99 - 10.00
+     */
+
+    var myDouble = 31.00
+    var myNewDouble: Double = 31.99
+
+    println("My double var is $myDouble")
+    println("My new double is $myNewDouble")
+}
