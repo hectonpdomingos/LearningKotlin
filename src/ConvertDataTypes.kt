72010229e5a0fdcf8dd36f7d3d6d70ceb1c852d6
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-17 18:08:12 -0200

    Convert Data Types

--- src/ConvertDataTypes.kt
+++ src/ConvertDataTypes.kt
@@ -1,3 +1,10 @@
 fun main(args: Array<String>) {
-    
+    var myNumber: Int = 30
+    println("My number is $myNumber + 1")
+
+    println("My age is " + (myNumber + 1))
+
+    println("Converting myNumber in float " + myNumber.toFloat())
+    println("Converting myNumber in double " + myNumber.toDouble())
 }
+
