1db13ee2c5865a97e865bba2ff9c9561377c30ef
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-17 18:45:51 -0200

    Making Decisions

--- src/MakingDecisions.kt
+++ src/MakingDecisions.kt
@@ -1,3 +1,44 @@
 fun main(args: Array<String>) {
-    
+
+    var numberOne = 10
+    var numberTwo = 20
+
+    println("Returning booleans")
+
+    println(numberOne > numberTwo)
+
+    println(numberOne < numberTwo)
+
+    println(numberOne == numberTwo)
+
+    println(numberOne != numberTwo)
+
+
+    if (numberOne > numberTwo){
+        println("numberOne is bigger than numberTwo")
+    }else{
+        println("numberOne is not bigger than numberTwo")
+    }
+
+   println("simplify the if else")
+
+    if (numberOne > numberTwo) println("Yes is bigger") else println("No isn't")
+
+    println("If else in the common way")
+
+    var myChar = 'h'
+
+    if (myChar == 'u'){
+        println("My char is u")
+    }else if (myChar == 'i'){
+        println("My char is i")
+    }else {
+        println("The char is $myChar")
+    }
+
+
+
+
+
+
 }
