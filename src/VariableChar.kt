c94c67ff466686b7e3b4ab71bfcb4110109e7406
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-17 16:44:35 -0200

    Variable Char

--- src/VariableChar.kt
+++ src/VariableChar.kt
@@ -1,3 +1,12 @@
 fun main(args: Array<String>) {
-    
+    /*
+    Variable Char example: a - b - c
+    Do not forget to change " to ' to define the char
+     */
+    var myChar = 'H'
+    var newChar: Char? = null
+    newChar = 'H'
+
+    println("My name starts with the letter $myChar")
+    println("My name starts with the letter $newChar from newChar")
 }
