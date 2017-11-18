069aeab26613c5df645864f67b056e42b4ef0335
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-18 15:03:59 -0200

    Inheritance

---
+++ src/Animal.kt
@@ -0,0 +1,16 @@
+class Animal(){
+
+    var name: String? = null
+    var color: String? = null
+    var numLegs: Int? = null
+    //primary constructor
+    constructor(name: String, color: String, numLegs:Int): this(){
+        this.name = name
+        this.color = color
+        this.numLegs = numLegs
+    }
+}
+
+fun main(args: Array<String>) {
+
+}
