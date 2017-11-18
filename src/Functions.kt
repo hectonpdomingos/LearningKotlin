06ce8b08048c1e9b44b8eb8f187d94ad79b73b95
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-18 08:49:49 -0200

    Functions

--- src/Functions.kt
+++ src/Functions.kt
@@ -1,3 +1,25 @@
 fun main(args: Array<String>) {
     println("Function example")
+
+
+    var sum: Int =  add(2, 2)
+
+
+    println("result of the first function is $sum")
+
+    println("Second way to do function")
+
+    add2(5, 5)
+}
+
+//with return
+fun add(num1: Int, num2: Int): Int{
+    var sum = num1 + num2
+    return sum
+}
+
+//without return
+fun add2(num1: Int, num2: Int){
+    var sum = num1 + num2
+    println("Result of the second function is $sum")
 }
