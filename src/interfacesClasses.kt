5dfa3e4968406eba843bfb11115f4babc5c50772
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-18 21:29:41 -0200

    Interfaces

--- src/interfacesClasses.kt
+++ src/interfacesClasses.kt
@@ -1,6 +1,32 @@
 
+interface actions{
+    fun closeWindows()
+    fun openWindows()
+    fun browswerInternet()
+}
+
+class Dimitri: actions{
+    override fun closeWindows() {
+         println("Dima is closing the windows")
+        }
+
+    override fun openWindows() {
+        openWindows(){
+            println("")
+        }
+    }
+
+    override fun browswerInternet() {
+        super.browswerInternet()
+    }
+}
+
 
 
 fun main(args: Array<String>) {
+
+   var  n = Dimitri()
+    n.closeWindows()
+
 
 }
