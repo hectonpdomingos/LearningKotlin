c587bed13fee8cdb2763e5e9a32063cfa5dd3f2c
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-18 14:50:02 -0200

    Classes

--- src/Classes.kt
+++ src/Classes.kt
@@ -15,15 +15,46 @@ class company(name: String, founded: Int, numberEmployees: Double){
         this.numberEmployees = numberEmployees
     }
 
+    //Equivalence Get of Java
+
+    fun GetName(): String? {
+        return this.name
+    }
+    //Equivalence Set of Java
+    fun SetName(name: String) {
+        this.name = name
+    }
+
+
 }
+
+
+
 
 fun main(args: Array<String>) {
 
     //create a object "signUpCompany"
     var signUpCompany = company("Hecton Support", 2002, 2.999)
+    println("Company :" + signUpCompany.name + " | Year of foundation: " + signUpCompany.founded + " | Number of employees: " + signUpCompany.numberEmployees)
+
+    //if you want to set up the propriety here, just do it
+    signUpCompany.name = "Hecton Corporation"
+    //Showing the changes
+    println("Company :" + signUpCompany.name + " | Year of foundation: " + signUpCompany.founded + " | Number of employees: " + signUpCompany.numberEmployees)
+
     //you can create as many objects as you want
     var signUpCompany2 = company("Helena Support", 2014, 10.000)
-
-    println("Company :" + signUpCompany.name + " | Year of foundation: " + signUpCompany.founded + " | Number of employees: " + signUpCompany.numberEmployees)
     println("Company :" + signUpCompany2.name + " | Year of foundation: " + signUpCompany2.founded + " | Number of employees: " + signUpCompany2.numberEmployees)
+
+
+    //Using the equivalence Get from Java
+    println("Using the equivalence Get from Java")
+    println(signUpCompany.GetName())
+    //Using the equivalence Set from Java
+    println("Using the equivalence Set from Java - set Dimitri Corp in Name ")
+    signUpCompany.SetName("Dimitri Corp")
+    println(signUpCompany.GetName())
+
+
+
 }
