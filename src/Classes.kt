ffb49e64a8fceca4ca35638eed87184029a7a731
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-18 13:44:52 -0200

    Classes

--- src/Classes.kt
+++ src/Classes.kt
@@ -1,3 +1,26 @@
+// The class company has the name, founded and numberEmployees parameters/proprieties
+class company(name: String, founded: Int, numberEmployees: Double){
+
+
+    //proprieties
+    var name: String? = null
+    var founded: Int? = null
+    var numberEmployees: Double? = null
+
+
+    init { //create a initializer to proprieties
+        // this means THIS context ...the same could be company.name - company.founded
+        this.name = name
+        this.founded = founded
+        this.numberEmployees = numberEmployees
+    }
+
+}
+
 fun main(args: Array<String>) {
-    
+
+    //create a object "signUpCompany"
+    var signUpCompany = company("Hecton Support", 2002, 2.999)
+
+    println(signUpCompany.name)
 }
