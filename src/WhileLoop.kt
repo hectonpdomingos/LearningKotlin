669568785d27b5b332df5fad8ced36e163929950
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-17 22:49:56 -0200

    Do While loop

--- src/WhileLoop.kt
+++ src/WhileLoop.kt
@@ -1,3 +1,19 @@
 fun main(args: Array<String>) {
-    
+
+    var counter = 0
+
+    //Do While Loop
+
+    do{
+
+    }while(counter < 5)
+
+    println("The end of the Do While Loop")
+    // While loop
+
+    while (counter < 5){
+        println("Counting ... $counter")
+        counter++
+    }
+    println("The end of the While Loop")
 }
