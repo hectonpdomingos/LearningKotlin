6b60cb48243be0cc1a0ec4ba348837ed9a4ad28f
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-17 22:45:34 -0200

    For loop

--- src/ForLoop.kt
+++ src/ForLoop.kt
@@ -1,8 +1,21 @@
 fun main(args: Array<String>) {
 
+    //dynamically range
+    var limit = 10
+    for(count in 0..limit){
+        println(count)
+    }
 
-    for(item in 1..5){
-        println(item)
+    // Static range
+    for (count in 0..10){
+        println(count)
+    }
+
+    println("Type a limit for the loop")
+    var theLimit = readLine()!!.toInt()
+
+    for (myCount in 0..theLimit){
+        println("The Limit is $theLimit and we are in the count of $myCount")
     }
 
 }
