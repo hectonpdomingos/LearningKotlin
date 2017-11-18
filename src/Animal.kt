15aa60007d21d5778bbe24856dcaa04321b316fd
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-18 15:29:20 -0200

    Inheritance

--- src/Animal.kt
+++ src/Animal.kt
@@ -1,8 +1,7 @@
-/*
-Seeting the concept of inheritance 
- */
 
-class Animal(){
+//Every class in Kotlin is Final, so to allows the inheritance
+// needs to set as open
+open class Animal(){
 
     var name: String? = null
     var color: String? = null
@@ -12,9 +11,37 @@ class Animal(){
         this.name = name
         this.color = color
         this.numLegs = numLegs
+
+        //Show the Object
+        println("Name: " + this.name)
+        println("Color: " + this.color)
+        println("Legs: " + this.numLegs)
+
     }
 }
 
+/*
+Seting the concept of inheritance
+
+The class Lion() will get (inheritance) the Animal class/proprieties
+ */
+class Lion(): Animal(){
+}
+
 fun main(args: Array<String>) {
+    //Seting the values to the proprieties
+    println("Animal proprieties")
+    var animal = Animal("Dog", "Brown", 4)
+
+    println("Lion proprieties")
+    var lion = Lion()
+    lion.name = "Cimbar"
+    lion.color = "yellow"
+    lion.numLegs = 4
+
+    println("Name: " + lion.name)
+    println("Color" + lion.color)
+    println("Legs" + lion.numLegs)
+
 
 }
