eeaba06ed267a12761867ac636b39008ec839f43
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-19 11:28:35 -0200

    playing with Arrays

--- src/Arrays.kt
+++ src/Arrays.kt
@@ -6,16 +6,43 @@ fun main(args: Array<String>) {
     var myFirstArray = Array<Int>(5) {8}
 
     //Arrays Filled with static numbers
-    val numbers: IntArray = intArrayOf(10, 20, 30, 40, 50)
+    val values = arrayOf(1, 2, 3, 4, "Brazil", 'H')
+    for (index in values) {
+        println(index)
+    }
+
+
+
+    println("Add values in the indexes")
+    var count = 0
+    var emptyArray = arrayOfNulls<Int>(10)
+    for (item in 0..emptyArray.size){
+       //if (count <= emptyArray.size){
+           //emptyArray[count] = 2
+        emptyArray[count] = count
+
+        println("Value in the count" + count)
+        println("Value in the array " + emptyArray[count])
+        ++count
+
+       // }
+
+    }
+
+    //Showing the values in the array
+    for (item in emptyArray){
+        println(emptyArray[count])
+        ++count
+    }
+
+
+
+
 
   //  var mySecondArray = Array<String>(0, {i -> ""})
 
 
 
-    for (i in numbers) {
-
-       println(numbers[i])
-        }
 
 
     }
