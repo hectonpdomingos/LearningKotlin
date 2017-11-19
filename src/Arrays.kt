96e191505c5053ae09bca93134ab8117f5f58a6c
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-19 12:36:03 -0200

    playing with Arrays

--- src/Arrays.kt
+++ src/Arrays.kt
@@ -1,50 +1,44 @@
 
-
 fun main(args: Array<String>) {
 
     //Array with 5 blocks filled with the number 8
     var myFirstArray = Array<Int>(5) {8}
 
-    //Arrays Filled with static numbers
-    val values = arrayOf(1, 2, 3, 4, "Brazil", 'H')
+    println("################  Arrays Filled with static numbers  #####################")
+    val values = arrayOf(1, 2, 3, 4, "Brazil", 'H', "Dimitri")
     for (index in values) {
         println(index)
     }
 
-
-
-    println("Add values in the indexes")
+    println("################   Add values in the indexes ################")
     var count = 0
+    var fillThisArray = arrayOfNulls<Int>(10)
     var emptyArray = arrayOfNulls<Int>(10)
-    for (item in 0..emptyArray.size){
-       //if (count <= emptyArray.size){
-           //emptyArray[count] = 2
-        emptyArray[count] = count
-
-        println("Value in the count" + count)
-        println("Value in the array " + emptyArray[count])
-        ++count
-
-       // }
-
-    }
-
-    //Showing the values in the array
     for (item in emptyArray){
-        println(emptyArray[count])
-        ++count
-    }
-
-
-
-
-
-  //  var mySecondArray = Array<String>(0, {i -> ""})
-
-
-
-
+        emptyArray[count] = count
+        fillThisArray[count] = count
+        println("Added in the index: " + count + " the value of: " + emptyArray[count])
+        if (count <  9){
+            ++count
+        }
 
     }
+    println("End of FIRST Loop")
+
+    println("################  Showing the values in the fillThisArray  array ##################")
+    var count2 = 0
+    for (item2 in fillThisArray){
+        println(fillThisArray[count2])
+        if (count2 < fillThisArray.size){
+            ++count2
+
+        }
+
+    }
+    println("End of SECOND Loop")
+
+
+}
+
 
 
