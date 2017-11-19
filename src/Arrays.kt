ca855f83547cd2a2538d63a71acb82a2c7ddba73
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-19 10:56:13 -0200

    Arrays

---
+++ src/Arrays.kt
@@ -0,0 +1,23 @@
+
+
+fun main(args: Array<String>) {
+
+    //Array with 5 blocks filled with the number 8
+    var myFirstArray = Array<Int>(5) {8}
+
+    //Arrays Filled with static numbers
+    val numbers: IntArray = intArrayOf(10, 20, 30, 40, 50)
+
+  //  var mySecondArray = Array<String>(0, {i -> ""})
+
+
+
+    for (i in numbers) {
+
+       println(numbers[i])
+        }
+
+
+    }
+
+
