025db25a6821f800b30f9f934c35c77c732fe4d7
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-20 14:17:17 -0200

    HashMap

--- src/HashMap.kt
+++ src/HashMap.kt
@@ -1,3 +1,24 @@
 fun main(args: Array<String>) {
-    
+
+
+    var myFirstHashMap = HashMap<String, String>()
+    myFirstHashMap.put("Car", "Ferrari")
+    myFirstHashMap.put("Motorcycle", "CG TITAM")
+    myFirstHashMap.put("Airplane", "747")
+
+
+    //Output the value based on the key
+    println(myFirstHashMap.get("Airplane"))
+
+   //Outputting the key and the value
+    for (item in myFirstHashMap){
+        println(item.key)
+    }
+    //
+    for (item in myFirstHashMap){
+        println(item)
+    }
+
+
+
 }
