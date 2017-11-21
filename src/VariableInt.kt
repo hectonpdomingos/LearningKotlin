12a0e325e6ea1b50c431b67200a12d7e432ecba1
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-21 00:32:43 -0200

    varInt

--- src/VariableInt.kt
+++ src/VariableInt.kt
@@ -2,4 +2,8 @@ fun main(args: Array<String>) {
     var age:Int = 32
     var year:Int = 2018
      println("In the year $year I will be $age years old")
+
+
+    var sum: Int = age + year
+    println(sum)
 }
