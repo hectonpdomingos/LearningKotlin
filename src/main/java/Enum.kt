2751953a3e8d169c9129cf0ced07d3136dc4bd0a
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-23 11:19:08 -0200

    Enum example

--- src/main/java/Enum.kt
+++ src/main/java/Enum.kt
@@ -1,5 +1,7 @@
-//Fixed or static info. Collection of information
+import javafx.scene.paint.Color
 
+//Fixed or static info. Collection of information
+//  InKotlin, enum isa so-called soft keyword:
 enum class Suits{
     HEARTS,
     SPADES,
@@ -12,4 +14,19 @@ fun main(args: Array<String>) {
     var player = Suits.DIAMONDS
     if(player == Suits.DIAMONDS) println("ok")
 
+    println(getMnemonic(Color.BLUE))
 }
+
+fun getMnemonic(color: Color) =
+        when (color) {
+            Color.RED -> "Richard"
+            Color.ORANGE -> "Of"
+            Color.YELLOW -> "York"
+            Color.GREEN -> "Gave"
+            Color.BLUE -> "Battle"
+            Color.INDIGO -> "In"
+            Color.VIOLET -> "Vain"
+            else -> {
+                println("Select the right color")
+            }
+        }
