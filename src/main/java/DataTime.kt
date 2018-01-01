4e0e7c2c792f7165baf42644c8c7b6910d82f7ce
Hecton Domingos <hectonpdomingos@gmail.com>
2017-12-31 23:51:27 -0200

    dataTime

---
+++ src/main/java/DataTime.kt
@@ -0,0 +1,11 @@
+import com.mysql.cj.core.io.YearToDateValueFactory
+import java.time.Year
+import java.time.YearMonth
+import java.util.*
+import java.util.TimeZone.getTimeZone
+
+fun main(args: Array<String>) {
+    var yearMonth = YearMonth.now()
+    var currentYear = Year.now()
+
+}
